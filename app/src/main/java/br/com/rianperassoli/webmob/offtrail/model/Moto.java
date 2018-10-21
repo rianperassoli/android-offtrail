c524fa1967a70163398c07174e0588e834a11408
Rian Perassoli <rian.perassoli@gmail.com>
2018-10-21 18:40:31 -0300

    Finalização das classes dos modelos

---
+++ app/src/main/java/br/com/rianperassoli/webmob/offtrail/model/Moto.java
@@ -0,0 +1,65 @@
+package br.com.rianperassoli.webmob.offtrail.model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable
+public class Moto {
+
+    @DatabaseField(generatedId = true)
+    private Integer codigo;
+
+    @DatabaseField(canBeNull = false)
+    private String modelo;
+
+    @DatabaseField
+    private String marca;
+
+    @DatabaseField
+    private String cin;
+
+    @DatabaseField
+    private String cor;
+
+    public Moto(){}
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
---
+++ app/src/main/java/br/com/rianperassoli/webmob/offtrail/model/Trilheiro.java
@@ -0,0 +1,54 @@
+package br.com.rianperassoli.webmob.offtrail.model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable
+public class Trilheiro {
+
+    @DatabaseField(generatedId = true)
+    private Integer codigo;
+
+    @DatabaseField(canBeNull = false)
+    private String nome;
+
+    @DatabaseField(canBeNull = false)
+    private Integer ida;
+
+    @DatabaseField(foreign = true, foreignColumnName = "codigo")
+    private Moto moto;
+
+    public Trilheiro(){}
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIda() {
+        return ida;
+    }
+
+    public void setIda(Integer ida) {
+        this.ida = ida;
+    }
+
+    public Moto getMoto() {
+        return moto;
+    }
+
+    public void setMoto(Moto moto) {
+        this.moto = moto;
+    }
+}
